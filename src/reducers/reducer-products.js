c32513012cef6079f3e8d71c36d5aaf0706b2f3e
Katarzyna Da Silva <conic916@gmail.com>
2019-09-05 17:28:40 +0200

    Add new name for img

---
+++ src/reducers/reducer-products.js
@@ -0,0 +1,84 @@
+{
+    [
+        {
+            img: "../../image/basket-heart.jpg",
+            id: '1',
+            name: 'Ciao',
+            AddInfo: 'New',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+
+        {
+            img: "../../image/basket-dark.jpg",
+            id: '2',
+            name: 'Ciao',
+            AddInfo: 'New',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+
+        {
+            img: "../../image/basket-royale.jpg",
+            id: '3',
+            name: 'Ciao',
+            AddInfo: 'New',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+
+        {
+            img: "../../image/basket-mamma.jpg",
+            id: '4',
+            name: 'Ciao',
+            AddInfo: 'New',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+
+        {
+            img: "../../image/basket-royal-dg.jpg",
+            id: '5',
+            name: 'Ciao',
+            AddInfo: 'New',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+
+        {
+            img: "../../image/basket-dg-white.jpg",
+            id: '6',
+            name: 'Ciao',
+            AddInfo: 'New',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+
+        {
+            img: "../../image/basket-flowers.jpg",
+            id: '7',
+            name: 'Ciao',
+            AddInfo: 'Last',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+
+        {
+            img: "../../image/basket-dg.jpg",
+            id: '8',
+            name: 'Ciao',
+            AddInfo: 'Sale',
+            price: '125.00',
+            Description: 'blablablablabla',
+            quantity: 1
+        },
+    ]
+
+}
